08454809dc5a85188e60cee35e808a8bde43f6f5
luisgiraldo <luisgiraldo@titamedia.com>
2024-09-08 12:59:43 -0500

    chore(./PopupsProvider): pruebas flujo CI

--- PopupsProvider/src/lib/PopupsProvider.tsx
+++ PopupsProvider/src/lib/PopupsProvider.tsx
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 import { PopupButtonProps } from '../types/types';
 import usePopup from '@apps/main-app/app/hooks/usePopup/UsePopup';
 
-
 const PopupButtons = ({ id, title, description, defaultPosition }:PopupButtonProps) => {
   const { addPopup } = usePopup();
 
@@ -17,6 +16,5 @@ const PopupButtons = ({ id, title, description, defaultPosition }:PopupButtonPro
     <button onClick={handleClick}>Abrir {title}</button>
   )
 }
-
 export const PopupButtonA = (props: Omit<PopupButtonProps, 'id' >) => <PopupButtons id={uuidv4()}  {...props} />;
 export const PopupButtonB = (props: Omit<PopupButtonProps, 'id' >) => <PopupButtons id={uuidv4()}  {...props} />;
