c4b09a31160bae8eb9adcd7e6404b076b3615b49
luisgiraldo <luisgiraldo@titamedia.com>
2024-09-09 18:21:07 -0500

    pruebas CD

--- apps/main-app/src/app/components/Modal/Modal.tsx
+++ apps/main-app/src/app/components/Modal/Modal.tsx
@@ -7,12 +7,11 @@ import { IoClose } from "react-icons/io5";
 const Modal = ({ isOpen, onClose, children }:ModalProps) => {
   if (!isOpen) return null;
 
-
   return (
     <div className={style.modal__container}>
       <div className={style.content__modal}>
         <div className={style.header__modal}>
-          <h2>Crea tu popup personalizada</h2>
+          <h2>Crea tu popup personalizada jajaja</h2>
           <IoClose onClick={onClose}/>
         </div>
         {children}
