2db17ad738764efa36d627b136eab12897c271e4
luisgiraldo <luisgiraldo@titamedia.com>
2024-09-09 23:27:02 -0500

    pruebas CI

--- apps/main-app/src/app/components/Modal/Modal.tsx
+++ apps/main-app/src/app/components/Modal/Modal.tsx
@@ -10,7 +10,7 @@ const Modal = ({ isOpen, onClose, children }:ModalProps) => {
     <div className={style.modal__container}>
       <div className={style.content__modal}>
         <div className={style.header__modal}>
-          <h2>Crea tu popup personalizada</h2>
+          <h2>Crea un popup personalizada</h2>
           <IoClose onClick={onClose}/>
         </div>
         {children}
